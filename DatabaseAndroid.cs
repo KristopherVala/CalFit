2cd929cb2adb2e7f9f07a102b09389d12756f8f6
Kristopher <kristophervala@gmail.com>
2020-04-21 09:40:24 -0400

    Add files via upload

---
+++ DatabaseAndroid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SQLite;
+using System.IO;
+using MobileFitnessCalorieTracker.Droid;
+
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseAndroid))]
+
+namespace MobileFitnessCalorieTracker.Droid
+{
+   public class DatabaseAndroid : ISQLiteDb
+    {
+		public SQLiteAsyncConnection GetConnection()
+		{
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MobileFitness.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+
+	}
+}   
---
+++ DatabaseIOS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MobileFitnessCalorieTracker.iOS;
+using SQLite;
+using System.IO;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseIOS))]
+
+namespace MobileFitnessCalorieTracker.iOS
+{
+    public class DatabaseIOS : ISQLiteDb
+    {
+
+            public SQLiteAsyncConnection GetConnection()
+            {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MobileFitness.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+
+        }
+    }
